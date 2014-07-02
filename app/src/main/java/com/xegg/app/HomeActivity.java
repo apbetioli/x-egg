c8784decab87d84a05efc34864738a7451df806a
Thiago <thiagomarquesgoes@gmail.com>
2014-07-01 22:10:30 -0300

    menu inicial

--- app/src/main/java/com/xegg/app/HomeActivity.java
+++ app/src/main/java/com/xegg/app/HomeActivity.java
@@ -1,8 +1,11 @@
 package com.xegg.app;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
 
@@ -12,6 +15,35 @@ public class HomeActivity extends BaseActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+      final LinearLayout l = (LinearLayout) findViewById(R.id.btn1);
+
+        l.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), ImagePagerActivity.class);
+                intent.putExtra(TAG, "programming"); //TODO pegar da categoria selecionada na tela
+                startActivity(intent);
+            }
+
+
+            public boolean onTouch(View v, MotionEvent event) {
+                // TODO Auto-generated method stub
+                switch(event.getAction())
+                {
+                    case MotionEvent.ACTION_DOWN:
+                        l.setBackgroundColor(Color.RED);
+                        break;
+                    case MotionEvent.ACTION_UP:
+
+                        //set color back to default
+                        l.setBackgroundColor(Color.WHITE);
+                        break;
+                }
+                return true;
+            }
+        } );
     }
 
     @Override
