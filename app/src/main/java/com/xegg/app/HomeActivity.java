7bc16b734cf10ed2c00212a9807d2835b1c017a0
Thiago <thiagomarquesgoes@gmail.com>
2014-07-05 00:41:09 -0300

    criando menu dinamico

--- app/src/main/java/com/xegg/app/HomeActivity.java
+++ app/src/main/java/com/xegg/app/HomeActivity.java
@@ -1,13 +1,29 @@
 package com.xegg.app;
 
+import android.app.ActionBar;
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static com.xegg.app.R.drawable.green;
+import static com.xegg.app.R.drawable.red;
+import static com.xegg.app.R.drawable.green;
+import static com.xegg.app.R.drawable.blue;
 
 public class HomeActivity extends BaseActivity {
 
@@ -15,35 +31,59 @@ public class HomeActivity extends BaseActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        final LinearLayout main = (LinearLayout) findViewById(R.id.principal);
+        List<String> tags = new ArrayList<String>();
+        tags.add("programming");
+        tags.add("cats");
+        tags.add("dogs");
+        tags.add("girls");
+        tags.add("world cup");
+        tags.add("NSFW");
+        tags.add("cosplay");
+        tags.add("food");
+        tags.add("Comic");
+        tags.add("WTF");
+        tags.add("gif");
+        int color[] = {red, blue, green};
 
-      final LinearLayout l = (LinearLayout) findViewById(R.id.btn1);
+        for (String tag : tags) {
+            final String tagRox = tag;
+            LinearLayout child = new LinearLayout(this);
+            child.setOrientation(LinearLayout.HORIZONTAL);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 220);
 
-        l.setOnClickListener(new View.OnClickListener() {
+            params.setMargins(0, 4, 0, 0);
+            Random random = new Random();
+            child.setLayoutParams(params);
+            child.setBackground(getResources().getDrawable(color[random.nextInt(color.length)]));
+            child.setBaselineAligned(true);
 
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), ImagePagerActivity.class);
-                intent.putExtra(TAG, "programming"); //TODO pegar da categoria selecionada na tela
-                startActivity(intent);
-            }
+            TextView tv1 = new TextView(this);
+            params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT);
+            params.setMargins(0, 6, 0, 0);
+            tv1.setLayoutParams(params);
+            tv1.setTextSize(40);
+            tv1.setTypeface(null, Typeface.BOLD);
+            tv1.setAllCaps(true);
+            tv1.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
+            tv1.setTextColor(getResources().getColor(R.color.white));
+            tv1.setText(tagRox);
+            child.addView(tv1);
 
+            main.addView(child);
 
-            public boolean onTouch(View v, MotionEvent event) {
-                // TODO Auto-generated method stub
-                switch(event.getAction())
-                {
-                    case MotionEvent.ACTION_DOWN:
-                        l.setBackgroundColor(Color.RED);
-                        break;
-                    case MotionEvent.ACTION_UP:
+            child.setOnClickListener(new View.OnClickListener() {
 
-                        //set color back to default
-                        l.setBackgroundColor(Color.WHITE);
-                        break;
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(v.getContext(), ImagePagerActivity.class);
+                    intent.putExtra(TAG, tagRox); //TODO pegar da categoria selecionada na tela
+                    startActivity(intent);
                 }
-                return true;
-            }
-        } );
+            });
+        }
+
     }
 
     @Override
