c2ece8b9f4331e79f7b11366d047c0114e67b28a
apbetioli <apbetioli@gitlab.com>
2014-07-23 12:58:40 -0300

    Animando o loading da imagem

--- app/src/main/java/com/xegg/app/core/XEggImageViewer.java
+++ app/src/main/java/com/xegg/app/core/XEggImageViewer.java
@@ -1,5 +1,9 @@
 package com.xegg.app.core;
 
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.CycleInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 
 import com.koushikdutta.ion.Ion;
@@ -15,11 +19,19 @@ public class XEggImageViewer {
     }
 
     public void loadImageFromPost(Post post) {
+        RotateAnimation rotateAnimation = new RotateAnimation(0, 360,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        rotateAnimation.setRepeatCount(Animation.INFINITE);
+        rotateAnimation.setRepeatMode(Animation.INFINITE);
+        rotateAnimation.setDuration(5000);
+
+        AlphaAnimation fadeInAnimation = new AlphaAnimation(0, 1);
+
         Ion.with(imageView)
                 .placeholder(R.drawable.ic_stub)
                 .error(R.drawable.ic_error)
-//                .animateLoad(R.drawable.ic_loading)
-//                .animateIn(fadeInAnimation)
+                .animateLoad(rotateAnimation)
+                .animateIn(fadeInAnimation)
                 .load(post.getImage());
     }
 
