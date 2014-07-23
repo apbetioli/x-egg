c736b0c8112cd07d9ac6b0bc0e387c2dee6c13d5
apbetioli <apbetioli@gitlab.com>
2014-07-23 13:11:33 -0300

    Metodo para validar content type da imagem - TODO

--- app/src/main/java/com/xegg/app/core/XEggApiClient.java
+++ app/src/main/java/com/xegg/app/core/XEggApiClient.java
@@ -4,13 +4,21 @@ import android.content.Context;
 
 import com.google.gson.reflect.TypeToken;
 import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.async.http.libcore.RawHeaders;
+import com.koushikdutta.ion.HeadersCallback;
 import com.koushikdutta.ion.Ion;
 import com.xegg.app.model.Post;
 import com.xegg.app.model.Tag;
 import com.xegg.app.util.Constants;
 
-import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpHead;
+import org.apache.http.impl.client.DefaultHttpClient;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.List;
@@ -68,4 +76,14 @@ public class XEggApiClient {
                 .setCallback(future);
     }
 
+    //TODO
+    public boolean test() throws IOException {
+
+        String url = "";
+
+        HttpClient client = new DefaultHttpClient();
+        HttpResponse response = client.execute(new HttpHead(url));
+        return response.getFirstHeader("Content-Type").toString().startsWith("image/");
+
+    }
 }
